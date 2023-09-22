bb7ed0d74dbca17adccde2ae154e49ddd4f87d22
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-22 08:32:35 +0000

    Update fs2, upickle to 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
-  lazy val sttp = "com.softwaremill.sttp.client3" %% "fs2" % "3.8.16"
-  lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % "3.8.16"
+  lazy val sttp = "com.softwaremill.sttp.client3" %% "fs2" % "3.9.0"
+  lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % "3.9.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
 }
