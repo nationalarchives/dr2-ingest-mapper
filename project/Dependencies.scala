df308240d32040a4c5a4fb5774bd1a317f4dc246
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-25 08:32:02 +0000

    Update scalatest to 3.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,5 +16,5 @@ object Dependencies {
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
   lazy val sttp = "com.softwaremill.sttp.client3" %% "fs2" % "3.8.16"
   lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % "3.8.16"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
 }
