f229d1484f8ae0f83cad29ab183a64669feaa658
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-25 08:31:22 +0000

    Update log4j-core, ... to 2.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 object Dependencies {
-  lazy val logbackVersion = "2.20.0"
+  lazy val logbackVersion = "2.22.0"
   lazy val awsClientVersion = "0.1.16"
   lazy val fs2Reactive = "co.fs2" %% "fs2-reactive-streams" % "3.7.0"
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
