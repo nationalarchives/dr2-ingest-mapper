8e286b162904797c25a98b5579d428f1b9a897a3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-02-10 08:23:30 +0000

    Update fs2, upickle to 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
   lazy val log4jTemplateJson = "org.apache.logging.log4j" % "log4j-layout-template-json" % logbackVersion
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
-  lazy val log4CatsCore= "org.typelevel" %% "log4cats-core" % log4CatsVersion;
+  lazy val log4CatsCore = "org.typelevel" %% "log4cats-core" % log4CatsVersion;
   lazy val log4CatsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.5"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.5"
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.30"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
-  lazy val sttp = "com.softwaremill.sttp.client3" %% "fs2" % "3.9.2"
-  lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % "3.9.2"
+  lazy val sttp = "com.softwaremill.sttp.client3" %% "fs2" % "3.9.3"
+  lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % "3.9.3"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
 }
