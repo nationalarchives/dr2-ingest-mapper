e91d1401575275dab3f92c4f274fae1228ae3239
techncl <nigel.lutale@nationalarchives.gov.uk>
2024-02-19 12:41:46 +0000

    Remove duplication from TestUtils

--- src/test/scala/uk/gov/nationalarchives/LambdaTest.scala
+++ src/test/scala/uk/gov/nationalarchives/LambdaTest.scala
@@ -8,8 +8,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import uk.gov.nationalarchives.Lambda._
 import uk.gov.nationalarchives.MetadataService._
-import uk.gov.nationalarchives.testUtils.TestUtils._
 import uk.gov.nationalarchives.testUtils.LambdaTestTestUtils
+import uk.gov.nationalarchives.testUtils.TestUtils.{DynamoRequestBody, DynamoTable}
 import upickle.default
 import upickle.default._
 
@@ -34,13 +34,14 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
   val dynamoServer = new WireMockServer(9005)
   val discoveryServer = new WireMockServer(9004)
 
+  private val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
+  import testUtils._
+
   "handleRequest" should "return the correct values from the lambda" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    val uuids = testUtils.uuids
-    val (folderIdentifier, assetIdentifier, _, _, _, _) = testUtils.stubValidNetworkRequests()
+    val (folderIdentifier, assetIdentifier, _, _, _, _) = stubValidNetworkRequests()
 
     val os = new ByteArrayOutputStream()
-    testUtils.IngestMapperTest().handleRequest(testUtils.defaultInputStream, os, null)
+    IngestMapperTest().handleRequest(defaultInputStream, os, null)
     val stateData = read[StateOutput](os.toByteArray.map(_.toChar).mkString)
     val archiveFolders = stateData.archiveHierarchyFolders
     archiveFolders.size should be(3)
@@ -54,25 +55,23 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
   }
 
   "handleRequest" should "write the correct values to dynamo" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    val uuids = testUtils.uuids
-    val (folderIdentifier, assetIdentifier, docxIdentifier, metadataIdentifier, originalFiles, originalMetadataFiles) = testUtils.stubValidNetworkRequests()
+    val (folderIdentifier, assetIdentifier, docxIdentifier, metadataIdentifier, originalFiles, originalMetadataFiles) = stubValidNetworkRequests()
     val os = new ByteArrayOutputStream()
-    testUtils.IngestMapperTest().handleRequest(testUtils.defaultInputStream, os, null)
+    IngestMapperTest().handleRequest(defaultInputStream, os, null)
     val dynamoRequestBodies = dynamoServer.getAllServeEvents.asScala.map(e => read[DynamoRequestBody](e.getRequest.getBodyAsString))
     dynamoRequestBodies.length should equal(1)
     val tableRequestItems = dynamoRequestBodies.head.RequestItems.test
 
     tableRequestItems.length should equal(6)
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable("TEST", UUID.fromString(uuids.head), "", "A", ArchiveFolder, "Test Title A", "TestDescriptionA with 0", Some("A"))
     )
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable("TEST", UUID.fromString(uuids.tail.head), uuids.head, "A 1", ArchiveFolder, "Test Title A 1", "TestDescriptionA 1 with 0", Some("A 1"))
     )
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable(
         "TEST",
@@ -86,7 +85,7 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
         customMetadataAttribute2 = Option("customMetadataValue2")
       )
     )
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable(
         "TEST",
@@ -103,7 +102,7 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
         originalMetadataFiles = originalMetadataFiles
       )
     )
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable(
         "TEST",
@@ -118,7 +117,7 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
         customMetadataAttribute1 = Option("customMetadataValue1")
       )
     )
-    testUtils.checkDynamoItems(
+    checkDynamoItems(
       tableRequestItems,
       DynamoTable(
         "TEST",
@@ -137,24 +136,22 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
   }
 
   "handleRequest" should "return an error if the discovery api is unavailable" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    testUtils.stubValidNetworkRequests()
+    stubValidNetworkRequests()
     discoveryServer.stop()
     val os = new ByteArrayOutputStream()
     val ex = intercept[Exception] {
-      testUtils.IngestMapperTest().handleRequest(testUtils.defaultInputStream, os, null)
+      IngestMapperTest().handleRequest(defaultInputStream, os, null)
     }
 
     ex.getMessage should equal("Exception when sending request: GET http://localhost:9004/API/records/v1/collection/A")
   }
 
   "handleRequest" should "return an error if the input files are not stored in S3" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    testUtils.stubValidNetworkRequests()
+    stubValidNetworkRequests()
     val inJson =
       s"""{
          |  "batchId": "TEST",
-         |  "s3Bucket": "${testUtils.inputBucket}",
+         |  "s3Bucket": "$inputBucket",
          |  "s3Prefix" : "INVALID/",
          |  "department": "A",
          |  "series": "A 1"
@@ -162,29 +159,27 @@ class LambdaTest extends AnyFlatSpec with MockitoSugar with BeforeAndAfterEach {
     val is = new ByteArrayInputStream(inJson.getBytes())
     val os = new ByteArrayOutputStream()
     val ex = intercept[Exception] {
-      testUtils.IngestMapperTest().handleRequest(is, os, null)
+      IngestMapperTest().handleRequest(is, os, null)
     }
 
     ex.getMessage should equal("null (Service: S3, Status Code: 404, Request ID: null)")
   }
 
   "handleRequest" should "return an error if the dynamo table doesn't exist" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    testUtils.stubValidNetworkRequests("invalidTable")
+    stubValidNetworkRequests("invalidTable")
     val os = new ByteArrayOutputStream()
     val ex = intercept[Exception] {
-      testUtils.IngestMapperTest().handleRequest(testUtils.defaultInputStream, os, null)
+      IngestMapperTest().handleRequest(defaultInputStream, os, null)
     }
 
     ex.getMessage should equal("Service returned HTTP status code 404 (Service: DynamoDb, Status Code: 404, Request ID: null)")
   }
 
   "handleRequest" should "return an error if the bag files from S3 are invalid" in {
-    val testUtils = new LambdaTestTestUtils(dynamoServer, s3Server, discoveryServer)
-    testUtils.stubInvalidNetworkRequests()
+    stubInvalidNetworkRequests()
     val os = new ByteArrayOutputStream()
     val ex = intercept[Exception] {
-      testUtils.IngestMapperTest().handleRequest(testUtils.defaultInputStream, os, null)
+      IngestMapperTest().handleRequest(defaultInputStream, os, null)
     }
 
     ex.getMessage should equal("Expected ujson.Arr (data: {})")
